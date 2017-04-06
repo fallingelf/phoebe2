fallingelf/phoebe2

commit 23e62b9845a56bd341ad69c3d44353161fc4891f

fixed bug introduced by last commit
I was wrong... returning None for parent_of(top_level_orbit) is required to avoid an infinite loop in dynamics.  Instead, I have implemented a simple if statement in the is_binary logic to handle top-level correctly

phoebe/parameters/parameters.py
@@ -4662,8 +4662,8 @@ class HierarchyParameter(StringParameter):
 
         # print "*** get_parent_of", component, trace
         if len(trace)<=1:
-            return self.get_top()
-            # return None
+            # return self.get_top()
+            return None
 
         return str(self._get_by_trace(structure, trace[:-2]+[trace[-2]-1]).split(':')[-1])
 
@@ -4863,7 +4863,11 @@ class HierarchyParameter(StringParameter):
             # TODO: is this the best fallback?
             return True
 
-        return self.get_kind_of(self.get_parent_of(component))=='orbit'
+        parent = self.get_parent_of(component)
+        if parent is None:
+            return True
+        else:
+            return self.get_kind_of(self.get_parent_of(component))=='orbit'
 
     def is_binary(self, component):
         """

commit 1776b6d39515eb4b1764bd6fcb03b24b120b0148

fixed bug in caching is_contact_binary
similar to 23e62b9845a56bd341ad69c3d44353161fc4891f
caused by fb6462da91d11348b89cd0817582a55f37cab167

phoebe/dynamics/nbody.py
@@ -111,8 +111,8 @@ def dynamics_from_bundle(b, times, compute=None, return_roche_euler=False, use_k
     # vgamma = b.get_value('vgamma', context='system', unit=u.AU/u.d) # should be extracted by keplerian.dynamics_from_bundle
     t0 = b.get_value('t0', context='system', unit=u.d)
 
-    if not _can_rebound and not conf.devel():
-        # NOTE: the conf.devel() only needs to be in place until rebound releases v 3.3.2
+    if not _can_rebound and not conf.devel:
+        # NOTE: the conf.devel only needs to be in place until rebound releases v 3.3.2
         raise ImportError("rebound 3.3.2+ is not installed")
 
     if gr and not _can_reboundx:

phoebe/parameters/parameters.py
@@ -4842,7 +4842,14 @@ class HierarchyParameter(StringParameter):
             # an envelope?
             return False
 
-        return self.get_kind_of(component)=='envelope' or (self.get_sibling_of(component, kind='envelope') is not None)
+        kind = self.get_kind_of(component)
+
+        if kind == 'envelope':
+            return True
+        elif kind=='orbit':
+            return False
+        else:
+            return self.get_sibling_of(component, kind='envelope') is not None
 
     def is_contact_binary(self, component):
         """
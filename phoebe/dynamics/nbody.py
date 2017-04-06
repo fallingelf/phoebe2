1776b6d39515eb4b1764bd6fcb03b24b120b0148
Kyle Conroy <kyleconroy@gmail.com>
2017-04-06 16:01:43 -0400

    fixed bug in caching is_contact_binary
    similar to 23e62b9845a56bd341ad69c3d44353161fc4891f
    caused by fb6462da91d11348b89cd0817582a55f37cab167

--- phoebe/dynamics/nbody.py
+++ phoebe/dynamics/nbody.py
@@ -111,8 +111,8 @@ def dynamics_from_bundle(b, times, compute=None, return_roche_euler=False, use_k
     # vgamma = b.get_value('vgamma', context='system', unit=u.AU/u.d) # should be extracted by keplerian.dynamics_from_bundle
     t0 = b.get_value('t0', context='system', unit=u.d)
 
-    if not _can_rebound and not conf.devel():
-        # NOTE: the conf.devel() only needs to be in place until rebound releases v 3.3.2
+    if not _can_rebound and not conf.devel:
+        # NOTE: the conf.devel only needs to be in place until rebound releases v 3.3.2
         raise ImportError("rebound 3.3.2+ is not installed")
 
     if gr and not _can_reboundx:
