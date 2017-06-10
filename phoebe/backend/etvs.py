9aa09062880f0f5faf94e2afd936d00448fb8c59
Kyle Conroy <kyleconroy@gmail.com>
2017-06-10 09:57:40 -0400

    minimize along plane of orbit (long_an)
    ... correctly this time

--- phoebe/backend/etvs.py
+++ phoebe/backend/etvs.py
@@ -20,16 +20,7 @@ def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-
     """
     tol in days
     """
-
-
-    def projected_separation_sq(time, b, dynamics_method, cind1, cind2, ltte=True):
-        """
-        """
-        #print "*** projected_separation_sq", time, dynamics_method, cind1, cind2, ltte
-
-
-        times = np.asarray(time)
-
+    def get_orbit(times, b, dynamics_method, cind1, cind2, ltte=True):
         if dynamics_method in ['nbody', 'rebound']:
             # TODO: make sure that this takes systemic velocity and corrects positions and velocities (including ltte effects if enabled)
             ts, xs, ys, zs, vxs, vys, vzs = dynamics.nbody.dynamics_from_bundle(b, times, compute=None, ltte=ltte)
@@ -44,20 +35,27 @@ def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-
         else:
             raise NotImplementedError
 
-        x1prime = xs[cind1][0] * np.cos(elongans[cind1][0])
-        x2prime = xs[cind2][0] * np.cos(elongans[cind2][0])
-        y1prime = ys[cind1][0] * np.sin(elongans[cind1][0])
-        y2prime = ys[cind2][0] * np.sin(elongans[cind2][0])
+        x1prime = xs[cind1][0] * np.cos(elongans[cind1][0]) + ys[cind1][0] * np.sin(elongans[cind1][0])
+        x2prime = xs[cind2][0] * np.cos(elongans[cind2][0]) + ys[cind2][0] * np.sin(elongans[cind2][0])
+        y1prime = -1 * xs[cind1][0] * np.sin(elongans[cind1][0]) + ys[cind1][0] * np.cos(elongans[cind1][0])
+        y2prime = -1 * xs[cind2][0] * np.sin(elongans[cind2][0]) + ys[cind2][0] * np.cos(elongans[cind2][0])
 
-        # print elongans[cind1][0], elongans[cind2][0]
-        # print x1prime, x2prime, y1prime, y2prime
-        # print times, (x2prime-x1prime)**2 + (y2prime-y1prime)**2
+        vx1prime = vxs[cind1][0] * np.cos(elongans[cind1][0]) + vys[cind1][0] * np.sin(elongans[cind1][0])
+        vx2prime = vxs[cind2][0] * np.cos(elongans[cind2][0]) + vys[cind2][0] * np.sin(elongans[cind2][0])
+        vy1prime = -1 * vxs[cind1][0] * np.sin(elongans[cind1][0]) + vys[cind1][0] * np.cos(elongans[cind1][0])
+        vy2prime = -1 * vxs[cind2][0] * np.sin(elongans[cind2][0]) + vys[cind2][0] * np.cos(elongans[cind2][0])
 
-        return (x2prime-x1prime)**2 + (y2prime-y1prime)**2
+        return ts, xs, ys, zs, vxs, vys, vzs, x1prime, x2prime, y1prime, y2prime, vx1prime, vx2prime, vy1prime, vy2prime
 
-        # return (np.cos(ethetas[cind1][0])-np.cos(ethetas[cind2][0]))**2
-        # return (xs[cind2][0]-xs[cind1][0])**2 + (ys[cind2][0]-ys[cind1][0])**2
-        # return (xs[cind2][0]-xs[cind1][0])**2
+
+    def projected_separation_sq(time, b, dynamics_method, cind1, cind2, ltte=True):
+        """
+        """
+        ts, xs, ys, zs, vxs, vys, vzs, x1prime, x2prime, y1prime, y2prime, vx1prime, vx2prime, vy1prime, vy2prime = get_orbit(np.asarray([time]), b, dynamics_method, cind1, cind2, ltte=ltte)
+
+        return (x2prime-x1prime)**2
+        # return (x2prime-x1prime)**2 + (y2prime-y1prime)**2
+        # return (xs[cind1][0]-xs[cind2][0])**2 + (ys[cind1][0]-ys[cind2][0])**2
 
 
     # TODO: optimize this by allowing to pass cind1 and cind2 directly (and fallback to this if they aren't)
@@ -67,19 +65,24 @@ def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-
 
     orb_period = b.get_value('period', component=b.hierarchy.get_parent_of(component), context='component')
 
+    ts, xs, ys, zs, vxs, vys, vzs, x1prime, x2prime, y1prime, y2prime, vx1prime, vx2prime, vy1prime, vy2prime = get_orbit(np.asarray([time]), b, dynamics_method, cind1, cind2, ltte=ltte)
+
+    # subtract because positive z is towards observer
+    guess = time - np.mean([zs[cind1][0], zs[cind2][0]])*_ltte_scale_factor
+    # print "***", time, guess
+
     # TODO: use z/c to estimate initial guess
     # guess = time + np.mean(zs)*_ltte_scale_factor
 
     # TODO: find the best minimizer (ie most efficient)
     # res = minimize(projected_separation_sq, x0=[time], method='TNC', args=(b, dynamics_method, cind1, cind2, ltte), options={'xtol': tol}, bounds=((time-orb_period/2., time+orb_period/2.),))
-    res = minimize(projected_separation_sq, x0=[time], method='TNC', args=(b, dynamics_method, cind1, cind2, ltte), options={'xtol': 1e-3, 'ftol': 1e-3}, bounds=((time-orb_period/2., time+orb_period/2.),))
+    res = minimize(projected_separation_sq, x0=[guess], method='TNC', args=(b, dynamics_method, cind1, cind2, ltte), options={'xtol': 1e-3, 'ftol': 1e-3}, bounds=((time-orb_period/2., time+orb_period/2.),))
     # res = minimize(projected_separation_sq, x0=[time], method='SLSQP', args=(b, dynamics_method, cind1, cind2, ltte), options={'ftol': tol}, bounds=((time-orb_period/2., time+orb_period/2.),))
     # res = minimize(projected_separation_sq, x0=[time], method='Nelder-Mead', args=(b, dynamics_method, cind1, cind2, ltte), options={'xatol': tol})
     # res = minimize(projected_separation_sq, x0=[time], method='Powell', args=(b, dynamics_method, cind1, cind2, ltte), options={'xtol': tol})
 
     if not res.success:
         logger.warning("Failed to find eclipse time near {} with msg: '{}'".format(time, res.message))
-        exit()
         return np.nan
 
     # manually check bounds (not necessary for minimizers that accept bounds)
